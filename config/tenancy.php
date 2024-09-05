591a54d194acd16bf01299e580ad7123c3433ace
Kamran Fareed <kamranfareed@bloomrix.com>
2024-09-05 15:55:34 +0500

    [update]: tenant prefix to suffix.

--- config/tenancy.php
+++ config/tenancy.php
@@ -50,8 +50,8 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'tenant',
-        'suffix' => '',
+        'prefix' => '',
+        'suffix' => '_tenant',
 
         /**
          * TenantDatabaseManagers are classes that handle the creation & deletion of tenant databases.
